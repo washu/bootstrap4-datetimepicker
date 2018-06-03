f20896942bfc3c32d842fa9522cb06f16560fdec
ankurk91 <pro.ankurk1@gmail.com>
2018-06-03 10:18:30 +0530

    Fix bootstrap 4 css class

--- src/js/bootstrap-datetimepicker.js
+++ src/js/bootstrap-datetimepicker.js
@@ -2342,7 +2342,7 @@
         if (element.hasClass('input-group')) {
             // in case there is more then one 'input-group-addon' Issue #48
             if (element.find('.datepickerbutton').length === 0) {
-                component = element.find('.input-group-addon');
+                component = element.find('.input-group-append');
             } else {
                 component = element.find('.datepickerbutton');
             }
